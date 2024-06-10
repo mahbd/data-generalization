b06502a6d328cec9cc813f74132393b2372b968a
Mahmudul Alam <mahmudula2000@gmail.com>
2024-06-10 16:06:49 +0600

    completed app

--- src/Home.tsx
+++ src/Home.tsx
@@ -43,7 +43,10 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    <div
+      data-theme="synthwave"
+      className="grid grid-cols-1 lg:grid-cols-2 gap-4"
+    >
       <div>
         <DataFileInput
           dataFile={dataFile}
